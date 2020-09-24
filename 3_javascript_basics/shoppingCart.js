7000c6fd565c0fa02ae0049d7129855a61015f18
Zsolt Toth <tothzs87@gmail.com>
2020-09-24 08:47:51 +0200

    shopping cart example

---
+++ 3_javascript_basics/shoppingCart.js
@@ -0,0 +1,32 @@
+var shoppingCart = [
+  {
+    id : 0,
+    product: 'apple',
+    quantity: 3,
+    unit_price: 10
+  },
+  {
+    id : 1,
+    product: 'banana',
+    quantity: 1,
+    unit_price: 23
+  },
+  {
+    id : 2,
+    product: 'cherry',
+    quantity: 6,
+    unit_price: 5
+  },
+  {
+    id : 3,
+    product: 'durian',
+    quantity: 100,
+    unit_price: 6
+  }
+]
+
+console.log('Total Cost: ???');
+console.log('Avg Cost per Product: ???');
+console.log('Most expensive product (unit price): ???');
+console.log('Most expensive prodcut (total): ???');
+
